e6dfd49878a5472f63de0188f937cfcef38e5729
wr1472 <wadud.ruf@gmail.com>
2021-11-13 15:36:57 +0000

    add git ignore.

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let mut x = 5;
+    println!("The value of x is: {}", x);
+    x = 6;
+    println!("The value of x is: {}", x);
+
+    let floored = 2.0 / 3.0; // Results in 0
+    println!("{}", floored);
 }
